7eaa19be473e33eaa11effe5b5f3f617d9a63b6b
Cow-Fu <nathanj758@gmail.com>
2016-06-09 17:03:30 -0400

    Added support for static refrences to the respective tracking classes

--- src/rocks/cow/Package/Carrier/CarrierType.java
+++ src/rocks/cow/Package/Carrier/CarrierType.java
@@ -1,18 +1,42 @@
 package rocks.cow.Package.Carrier;
 
+import rocks.cow.Tracker.Tracker;
+import rocks.cow.Tracker.Trackers.UpsTracker;
+
 import java.util.Optional;
 
 public enum CarrierType {
-    UPS("UPS", "https://wwwapps.ups.com/WebTracking/processInputRequest?&sort_by=status&tracknums_displayed=1&TypeOfInquiryNumber=T&loc=en_US&InquiryNumber1="),
-    USPS("USPS", "https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1="),
-    FEDEX("FEDEX", "Fillter");
+    UPS(
+            "UPS",
+            "https://wwwapps.ups.com/WebTracking/processInputRequest?&sort_by=status&tracknums_displayed=1&TypeOfInquiryNumber=T&loc=en_US&InquiryNumber1=",
+            UpsTracker.class
+    ),
+
+    USPS(
+            "USPS",
+            "https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1="
+    ),
+
+    FEDEX(
+            "FEDEX",
+            "Fillter"
+    );
 
     private String id;
     private String url;
+    private Class<? extends Tracker> aClass;
 
+    @Deprecated  // will be removed soon
     CarrierType(String id, String url) {
         this.id = id;
         this.url = url;
+        this.aClass = null;
+    }
+
+    CarrierType(String id, String url, Class<? extends Tracker> aClass) {
+        this.id = id;
+        this.url = url;
+        this.aClass = aClass;
     }
 
     public String getID() {
