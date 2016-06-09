917bf8bebc7c2cbe7a9f95f155c79139b8afc487
Cow-Fu <nathanj758@gmail.com>
2016-06-09 19:50:38 -0400

    started work on usps tracking

--- src/rocks/cow/Main.java
+++ src/rocks/cow/Main.java
@@ -2,25 +2,25 @@ package rocks.cow;
 
 import rocks.cow.Package.Carrier.CarrierType;
 import rocks.cow.Package.Package;
-//import rocks.cow.Tracker.Trackers.UspsTracker;
 import rocks.cow.Tracker.Trackers.UpsTracker;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
 
 
 public class Main {
     public static void main(String[] args) throws IOException, NoSuchMethodException {
-//        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
-//        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+       java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+       System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
 
         Package p = new Package("Fluffy", "1ZE6E4990277552394", CarrierType.UPS);
 
         HashMap<String, ArrayList<? extends String>> packageInfo = new UpsTracker().track(p);
 
-        System.out.println(packageInfo.get("date-time"));
+        System.out.println(packageInfo.get("dateTime"));
         System.out.println(packageInfo.get("status"));
-        System.out.println(packageInfo.get("locations"));
+        System.out.println(packageInfo.get("location"));
     }
 }
