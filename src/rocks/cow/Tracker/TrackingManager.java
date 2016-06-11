7ee5b78ba62e5e5fee9feb6b1805fc3f7683b1ca
Cow-Fu <nathanj758@gmail.com>
2016-06-10 21:21:14 -0400

    Added basic tracking management

---
+++ src/rocks/cow/Tracker/TrackingManager.java
@@ -0,0 +1,32 @@
+package rocks.cow.Tracker;
+
+import rocks.cow.Package.Carrier.CarrierType;
+import rocks.cow.Package.Package;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TrackingManager {
+
+    public static HashMap<String, ArrayList<? extends String>> track(Package p){
+        Tracker tracker = null;
+
+        for (CarrierType type: CarrierType.values()) {
+            if (p.getCarrier().equals(type)) {
+                try {
+                    tracker = type.getTrackingClass().newInstance();
+                } catch (InstantiationException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+
+        if (tracker == null) {
+            throw new NullPointerException("Unable to find tracking class");
+        }
+
+        return tracker.track(p);
+    }
+
+}
