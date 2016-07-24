b0fe03f4c2a093beff4451cac3de68b30a696286
Cow-Fu <nathanj758@gmail.com>
2016-07-24 10:56:49 -0400

    Made package writer and reader to the correct task
    fixed spelling error

--- src/rocks/cow/PackageTracker/Util/Package/PackageReader.java
+++ src/rocks/cow/PackageTracker/Util/Package/PackageReader.java
@@ -1,25 +1,69 @@
 package rocks.cow.PackageTracker.Util.Package;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import rocks.cow.PackageTracker.Package.Carrier.CarrierType;
 import rocks.cow.PackageTracker.Package.Package;
 import rocks.cow.PackageTracker.Util.FileMethods;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class PackageReader {
-    private static Gson gson = new Gson();
-
-    public static void savePackages(String file, ArrayList<Package> packages) throws FileNotFoundException {
-        savePackages(new File(file), packages);
+    public ArrayList<Package> loadPackages(String file) throws FileNotFoundException {
+        return loadPackages(new File(file));
     }
 
-    public static void savePackages(File file, ArrayList<Package> packages) throws FileNotFoundException {
+    public ArrayList<Package> loadPackages(File file) throws FileNotFoundException {
+        ArrayList<Package> packages = new ArrayList<>();
+
         if (!file.exists()) {
             throw new FileNotFoundException();
         }
-        new FileMethods(file).write(gson.toJson(packages));
+
+        JsonElement json = new JsonParser().parse(new FileMethods(file).readFullFile());
+        if (json.isJsonNull()) {
+            throw new NullPointerException();
+        }
+
+        if (!json.isJsonArray() && !json.isJsonObject()) {
+            throw new Error("No JsonObjects or JsonArray's were found in " + file.getPath());
+
+        }
+
+        JsonObject jsonObj;
+        Optional<CarrierType> type;
+
+        if (json.isJsonArray()) {
+            for (JsonElement j: json.getAsJsonArray()) {
+                jsonObj = j.getAsJsonObject();
+                type = CarrierType.getType(jsonObj.get("carrier").getAsString());
+
+                if (!type.isPresent()) {
+                    throw new Error("Invalid Carrier Type");
+                }
+
+                packages.add(new Package(
+                        jsonObj.get("description").getAsString(),
+                        jsonObj.get("trackingId").getAsString(),
+                        type.get()
+                ));
+            }
+        } else if (json.isJsonObject()) {
+            jsonObj = json.getAsJsonObject();
+            type = CarrierType.getType(jsonObj.get("carrier").getAsString());
+
+            if (!type.isPresent()) {
+                throw new Error("Invalid Carrier Type");
+            }
+
+            packages.add(new Package(
+                    jsonObj.get("description").getAsString(),
+                    jsonObj.get("trackingId").getAsString(),
+                    type.get()
+            ));
+        }
+        return packages;
     }
 }
---
+++ src/rocks/cow/PackageTracker/Util/Package/PackageWriter.java
@@ -0,0 +1,24 @@
+package rocks.cow.PackageTracker.Util.Package;
+
+import com.google.gson.*;
+import rocks.cow.PackageTracker.Package.Package;
+import rocks.cow.PackageTracker.Util.FileMethods;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class PackageWriter {
+    private static Gson gson = new Gson();
+
+    public static void savePackages(String file, ArrayList<Package> packages) throws FileNotFoundException {
+        savePackages(new File(file), packages);
+    }
+
+    public static void savePackages(File file, ArrayList<Package> packages) throws FileNotFoundException {
+        if (!file.exists()) {
+            throw new FileNotFoundException();
+        }
+        new FileMethods(file).write(gson.toJson(packages));
+    }
+}
